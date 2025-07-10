cd84d9a8ef388ad193ca7039dd5a6a910d357fcc
AnIv <anton.ivashkevich@uzh.ch>
2025-07-10 16:09:39 +0200

    Revert "change to dev environment"
    This reverts commit 3d8f7cd5a663efa31b1d51a419704bd41213c5cf.

--- next.config.js
+++ next.config.js
@@ -2,18 +2,12 @@
 const nextConfig = {
   //output: 'export',
   // Remove or disable trailingSlash
-  basePath: process.env.NODE_ENV === 'production' ? '/handsonvoting' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/davinci-sdk' : '',
   images: {
     unoptimized: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    ignoreBuildErrors: true,
   },
   env: {
     API_URL: process.env.API_URL,
